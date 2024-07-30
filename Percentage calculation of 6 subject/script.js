eb6c7412dc7ea8a02068319ff4b20ee3d88628f4
BishalPandey <142412265+BishalPandey1@users.noreply.github.com>
2024-07-30 20:32:33 +0545

    Update script.js

--- Percentage calculation of 6 subject/script.js
+++ Percentage calculation of 6 subject/script.js
@@ -4,19 +4,15 @@ function calculatePercentages() {
   var overallMarksObtained = 0;
   var overallTotalMarks = 0;
   var individualResults = '';
-
   subjects.forEach(function(subject, index) {
     var subjectName = subject.querySelector('input[type="text"]').value;
     var marks = parseFloat(subject.querySelector('input[type="number"]').value);
     var total = parseFloat(subject.querySelectorAll('input[type="number"]')[1].value);
     var percentage = (marks / total) * 100;
-
     overallMarksObtained += marks;
     overallTotalMarks += total;
-
     individualResults += subjectName + ' Percentage: ' + percentage.toFixed(2) + '%<br>';
   });
-
   var overallPercentage = (overallMarksObtained / overallTotalMarks) * 100;
   document.getElementById('individualResults').innerHTML = individualResults;
   document.getElementById('overallResult').innerHTML = 'Overall Percentage: ' + overallPercentage.toFixed(2) + '%';
